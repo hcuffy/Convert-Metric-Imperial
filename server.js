937e5a5691ab83bfb01ae157bc618ce7cf350928
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-23 11:48:02 +0200

    Add server

---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./routes');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/conversion');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json('*/*'));
+app.use('/', routes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log('Express server running on port', port);
+});
