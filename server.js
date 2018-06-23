5355b4aeb5257c105c61254d473a6bf3651f9f5d
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-23 12:12:56 +0200

    AAdding error log.

--- server.js
+++ server.js
@@ -8,12 +8,24 @@ const app = express();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/conversion');
 
+function logErrors(err, req, res, next) {
+	console.error(err.stack);
+	next(err);
+}
+
+function errorHandler(err, req, res, next) {
+	res.status(500);
+	res.render('error', { error: err });
+}
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json('*/*'));
+app.use(logErrors);
+app.use(logErrors);
 app.use('/', routes);
 
 const port = process.env.PORT || 3000;
