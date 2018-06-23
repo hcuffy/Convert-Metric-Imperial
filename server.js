57368b91bb9dadd88beb3c01c64b4054528a1a7f
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-23 12:34:31 +0200

    Add erro handling

--- server.js
+++ server.js
@@ -8,25 +8,22 @@ const app = express();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/conversion');
 
-function logErrors(err, req, res, next) {
-	console.error(err.stack);
-	next(err);
-}
-
-function errorHandler(err, req, res, next) {
-	res.status(500);
-	res.render('error', { error: err });
-}
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json('*/*'));
-app.use(logErrors);
-app.use(logErrors);
 app.use('/', routes);
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	next(err);
+});
+app.use(function(err, req, res, next) {
+	res.status(500);
+	console.error(err.stack);
+	res.render('error', { error: err });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
