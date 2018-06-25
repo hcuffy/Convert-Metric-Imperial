8a81adc55a50094a6e19224e64b36d8f1343b66c
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-25 18:52:40 +0200

    Check number validity

--- controllers/conversions.js
+++ controllers/conversions.js
@@ -1,13 +1,17 @@
 exports.makeConversion = (req, res, next) => {
 	var input = req.query.data;
-	let firstNum = input.search(/\d/);
-	let firstLetter = input.search(/[a-zA-Z]/);
-	let num = input.slice(firstNum, firstLetter).trim();
-	let unit = input
+	const firstNum = input.search(/\d/);
+	const firstLetter = input.search(/[a-zA-Z]/);
+	const num = input.slice(firstNum, firstLetter).trim();
+	const unit = input
 		.slice(firstLetter)
 		.trim()
 		.toLowerCase();
-	console.log(unit);
-
+	// TODO: Add more validations
+	if (isNaN(num) || num == '') {
+		console.log('not a number');
+	}
+	console.log(num);
+	// TODO: Add conversion
 	res.render('index');
 };
