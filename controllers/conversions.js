f79ae1efdff9ff771dab6adc3b855606abcb292f
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-25 18:38:08 +0200

    Add trim

--- controllers/conversions.js
+++ controllers/conversions.js
@@ -2,8 +2,8 @@ exports.makeConversion = (req, res, next) => {
 	var input = req.query.data;
 	let firstNum = input.search(/\d/);
 	let firstLetter = input.search(/[a-zA-Z]/);
-	let num = input.slice(firstNum, firstLetter);
-	let unit = input.slice(firstLetter);
+	let num = input.slice(firstNum, firstLetter).trim();
+	let unit = input.slice(firstLetter).trim();
 	console.log(unit);
 
 	res.render('index');
