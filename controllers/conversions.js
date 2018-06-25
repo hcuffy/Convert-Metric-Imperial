0e6f11d4b1c02bf8430ef7668614bcd74a4c5a2b
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-25 18:36:44 +0200

    Extract unit

--- controllers/conversions.js
+++ controllers/conversions.js
@@ -2,9 +2,9 @@ exports.makeConversion = (req, res, next) => {
 	var input = req.query.data;
 	let firstNum = input.search(/\d/);
 	let firstLetter = input.search(/[a-zA-Z]/);
-
 	let num = input.slice(firstNum, firstLetter);
-	console.log(num);
+	let unit = input.slice(firstLetter);
+	console.log(unit);
 
 	res.render('index');
 };
