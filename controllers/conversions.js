332ce4e2d317b72f4475c746e5481b5c933039be
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-30 08:02:46 +0200

    Check for dots and slashes

--- controllers/conversions.js
+++ controllers/conversions.js
@@ -1,7 +1,7 @@
 exports.makeConversion = (req, res, next) => {
 	var input = req.query.data;
 	const firstNum = input.search(/\d/);
-	const firstLetter = input.search(/[a-zA-Z]/);
+	const firstLetter = input.search(/[a-zA-Z]+/);
 	const num = input.slice(firstNum, firstLetter).trim();
 	const unit = input
 		.slice(firstLetter)
@@ -11,7 +11,7 @@ exports.makeConversion = (req, res, next) => {
 	if (isNaN(num) || num == '') {
 		console.log('not a number');
 	}
-	console.log(num);
+	console.log(unit);
 	// TODO: Add conversion
 	res.render('index');
 };
