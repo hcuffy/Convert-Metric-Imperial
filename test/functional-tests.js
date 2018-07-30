4ed9edae83c59c712144e181f3789d7e93703047
Henry Cuffy <hcuffy_5@hotmail.com>
2018-07-30 19:50:27 +0200

    Fix test

--- test/functional-tests.js
+++ test/functional-tests.js
@@ -2,55 +2,76 @@ const chai = require('chai')
 const chaiHttp = require('chai-http')
 const assert = require('chai').assert
 const server = require('../server')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+
 chai.use(chaiHttp)
 
 describe('Testing functions',  ()  => {
-	it('convert liters to gallons', (done) =>   {
+
+
+	it('convert 30l as a valid input should work', (done) =>  {
 		chai.request(server)
 			.get('/api/convert')
-			.query({ data: '34l' })
+			.query({ data:'30l' })
 			.end((err, res) => {
+				const dom = new JSDOM(res.text)
+				let output = dom.window.document.body.querySelector('h4').textContent
 				assert.equal(res.status, 200)
-				done()
-			})
-	})
-	it('convert fraction liters to gallons', (done) =>  {
-		chai.request(server)
-			.get('/api/convert')
-			.query({ data:'3/4l' })
-			.end((err, res) => {
-				assert.equal(res.status, 200)
+				assert.equal(output , 'Results: 30 liters converts to 7.925165305739669 gallons')
 				done()
 			})
 	})
 
-	it('convert deciment kilometers to miles', (done) =>  {
+	it('convert invalid input(56w) should not work', (done) =>   {
 		chai.request(server)
 			.get('/api/convert')
-			.query({ data:'3.5km' })
+			.query({ data: '56w' })
 			.end((err, res) => {
+				const dom = new JSDOM(res.text)
+				let output = dom.window.document.body.querySelector('h4').textContent
 				assert.equal(res.status, 200)
+				assert.equal(output , 'Results: Invalid Unit')
+				done()
+			})
+	})
+	it('Invalid number input(4/9.4.3/4kg) should not work', (done) =>  {
+		chai.request(server)
+			.get('/api/convert')
+			.query({ data:'4/9.4.3/4kg' })
+			.end((err, res) => {
+				const dom = new JSDOM(res.text)
+				let output = dom.window.document.body.querySelector('h4').textContent
+				assert.equal(res.status, 200)
+				assert.equal(output , 'Results: Invalid number')
 				done()
 			})
 	})
 
-	it('convert franction-decimal gallon to liters', (done)  => {
+	it('Invalid number and unit input(4/9.4.3/4w) should not work', (done) =>  {
 		chai.request(server)
 			.get('/api/convert')
-			.query({ data:'3.4/3.5gal' })
+			.query({ data:'4/9.4.3/4w' })
 			.end((err, res) => {
+				const dom = new JSDOM(res.text)
+				let output = dom.window.document.body.querySelector('h4').textContent
 				assert.equal(res.status, 200)
+				assert.equal(output , 'Results: Invalid Unit & Number')
 				done()
 			})
 	})
 
-	it('convert pounds  to kilogram', (done) =>  {
+	it('conversions with no number shoul work', (done)  => {
 		chai.request(server)
 			.get('/api/convert')
-			.query({ data:'24lbs' })
+			.query({ data:'gal' })
 			.end((err, res) => {
+				const dom = new JSDOM(res.text)
+				let output = dom.window.document.body.querySelector('h4').textContent
 				assert.equal(res.status, 200)
+				assert.equal(output , 'Results: 1 gallons converts to 3.78541 liters')
 				done()
 			})
 	})
+
 })
